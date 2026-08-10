e6082b543b8d274030067b92e2094f02e1abb8d5
tinakariuki24-oss <tina.kariuki24@students.dkut.ac.ke>
2026-08-10 14:09:07 +0300

    Configure automated pytest workflow and dependencies

---
+++ test_main.py
@@ -0,0 +1,8 @@
+﻿from fastapi.testclient import TestClient
+from main import app 
+
+client = TestClient(app)
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code in [200, 404]
