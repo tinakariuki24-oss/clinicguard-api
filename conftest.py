b5658df462d09c838d99d1f9758e15726a92f494
tinakariuki24-oss <tina.kariuki24@students.dkut.ac.ke>
2026-08-10 14:38:56 +0300

    Add conftest fixture to automatically create database tables for tests

---
+++ conftest.py
@@ -0,0 +1,9 @@
+﻿import pytest
+from sqlmodel import SQLModel
+from database.session import engine
+
+@pytest.fixture(scope="session", autouse=True)
+def setup_database():
+    SQLModel.metadata.create_all(engine)
+    yield
+    SQLModel.metadata.drop_all(engine)
